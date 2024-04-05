1a00cdab574fa585ed92d6894cae1b0f830500f3
surilindur <surilindur@users.noreply.github.com>
2024-04-06 01:15:27 +0300

    Reduce gateway intents to minimum required

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from src.config import DISCORD_TOKEN, HISTORY_MINIMUM
 from src.client import Apparatus
 
 if __name__ == "__main__":
-    intents = Intents.default()
+    intents = Intents.none()
+    intents.guild_messages = True
+    intents.guilds = True
     intents.message_content = True
     intents.members = True
     client = Apparatus(intents=intents, max_messages=HISTORY_MINIMUM)
